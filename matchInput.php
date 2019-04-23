47ee12aaa09d72514e2a26992c1525c6ffc0f40a
JP-Vela <jangofett122003@gmail.com>
2019-04-23 16:30:34 -0700

    Fixed matchInput button bug

--- matchInput.php
+++ matchInput.php
@@ -3,9 +3,7 @@
 	include("navBar.php");
 ?>
 <script src="Orange-Rind/js/orangePersist.js"></script>
-<script src="matchInputButtons.js"></script>
 <script src="matchInputDynamic.js"></script>
-<!--<script src="matchInputPost.js"></script>-->
 <script>
 
 function postwith(to){
@@ -141,7 +139,7 @@ function postwith(to){
 						<div class="togglebutton" id="reach">
 							<h4 id="updateModeToggle"><b><u>Adding</u></b></h4>
 							<label>
-								<input id="updateMode" type="checkbox" onclick="changeMode()" checked>
+								<input id="updateMode" type="checkbox" onclick="changeMode();" checked>
 							</label>
 						</div>
 
@@ -251,7 +249,129 @@ function postwith(to){
 								updateMode=true;
 								updateModeToggle.innerHTML="<b><u>Adding</u></b>";
 							}
+							console.log("updateMode2: "+updateMode);
 						}
+					</script>
+
+					<script>
+					var increment = 1;
+					var updateMode2 = true;
+					var cargoShipCargo = 0;
+					var cargoShipHatch = 0;
+					var rocket1Cargo = 0;
+					var rocket1Hatch = 0;
+					var rocket2Cargo = 0;
+					var rocket2Hatch = 0;
+					var rocket3Cargo = 0;
+					var rocket3Hatch = 0;
+
+					var cargoShipHatchT = 0;
+					var cargoShipCargoT = 0;
+					var rocket1CargoT = 0;
+					var rocket1HatchT = 0;
+					var rocket2CargoT = 0;
+					var rocket2HatchT = 0;
+					var rocket3CargoT = 0;
+					var rocket3HatchT = 0;
+
+					//Auto Scouting
+					//Cargo Ship
+					function updateShipCargo(){
+						if(updateMode2){
+							cargoShipCargo += increment;
+						} else if(cargoShipCargo>0){
+								cargoShipCargo -= increment;
+						}
+						document.getElementById("cargoShipCargo").innerHTML=cargoShipCargo;
+
+					}
+
+					function updateShipHatch(){
+						if(updateMode2){
+							cargoShipHatch += increment;
+						} else if(cargoShipHatch>0){
+								cargoShipHatch -= increment;
+						}
+						document.getElementById("cargoShipHatch").innerHTML=cargoShipHatch;
+
+					}
+
+					function changeMode(){
+						var updateModeToggle = document.getElementById('updateModeToggle');
+
+						if(updateMode2){
+							updateMode2=false;
+							updateModeToggle.innerHTML="<b><u>Subtracting</u></b>";
+						} else{
+							updateMode2=true;
+							updateModeToggle.innerHTML="<b><u>Adding</u></b>";
+						}
+						console.log("updateMode2: "+updateMode2);
+					}
+
+					//Auto Experimental
+
+					//Rocket
+					function updateRocketL1Cargo(){
+						if(updateMode2){
+						rocket1Cargo += increment;
+						} else if(rocket1Cargo>0){
+								rocket1Cargo -= increment;
+						}
+						document.getElementById("rocket1Cargo").innerHTML=rocket1Cargo;
+
+					}
+
+					function updateRocketL1Hatch(){
+						if(updateMode2){
+						rocket1Hatch += increment;
+					} else if(rocket1Hatch>0){
+								rocket1Hatch -= increment;
+						}
+						document.getElementById("rocket1Hatch").innerHTML=rocket1Hatch;
+
+					}
+
+					function updateRocketL2Cargo(){
+						if(updateMode2){
+						rocket2Cargo += increment;
+					} else if(rocket2Cargo>0){
+								rocket2Cargo -= increment;
+						}
+						document.getElementById("rocket2Cargo").innerHTML=rocket2Cargo;
+
+					}
+
+					function updateRocketL2Hatch(){
+						if(updateMode2){
+						rocket2Hatch += increment;
+						} else if(rocket2Hatch>0){
+								rocket2Hatch -= increment;
+						}
+						document.getElementById("rocket2Hatch").innerHTML=rocket2Hatch;
+
+					}
+
+					function updateRocketL3Cargo(){
+						if(updateMode2){
+						rocket3Cargo += increment;
+					} else if(rocket3Cargo>0){
+								rocket3Cargo -= increment;
+						}
+						document.getElementById("rocket3Cargo").innerHTML=rocket3Cargo;
+
+					}
+
+					function updateRocketL3Hatch(){
+						if(updateMode2){
+						rocket3Hatch += increment;
+					} else if(rocket3Hatch>0){
+								rocket3Hatch -= increment;
+						}
+						document.getElementById("rocket3Hatch").innerHTML=rocket3Hatch;
+
+					}
+
 					</script>
 
 						<!--Cargo Ship-->
